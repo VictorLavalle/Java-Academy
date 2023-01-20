4982c1a7b50547e4d87d5304ccdd3994dcf5d276
Vic <vmlc1499@gmail.com>
2023-01-19 21:17:37 -0600

    💡 Added missing comments for the object class
    WIP

--- Week1/Exams/Exam1/src/com/academy/week1/exam/question1/Batman.java
+++ Week1/Exams/Exam1/src/com/academy/week1/exam/question1/Batman.java
@@ -3,24 +3,45 @@ package com.academy.week1.exam.question1;
 public class Batman extends Superhero {
 
     public Batman(String name, String superpower, int powerLevel) {
+        // Call the constructor of the superclass
         super(name, superpower, powerLevel);
     }
+
+    /*
+     * This method should return the secret identity
+     * of the superhero by overriding the method from
+     * the abstract class.
+     */
 
     @Override
     public String getSecretIdentity() {
         return "Bruce Wayne";
     }
 
+
+    /*
+     * This method should return the superpower of the superhero
+     * by overriding the method from the interface.
+     */
     @Override
     public String obtainSuperpower() {
         return getSuperpower();
     }
 
+    /*
+     * This method should return the power of the superhero
+     * by overriding the method from the interface.
+     */
     @Override
     public int calculatePower() {
         return getPowerLevel() * 100;
     }
 
+
+    /*
+     * This method should return a String representation of the superhero
+     * by overriding the method from the Object class.
+     */
     @Override
     public String toString() {
         return "Batman [getName()=" + getName()
