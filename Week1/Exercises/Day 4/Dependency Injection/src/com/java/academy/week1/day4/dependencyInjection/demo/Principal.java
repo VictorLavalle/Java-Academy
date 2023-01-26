b039ab19b9ab72232ef32be83db75ef7659d8965
Victor Lavalle <vmlc1499@gmail.com>
2023-01-26 10:47:51 -0600

    ♻️ Added Dependecy Injection demo code
    WIP

---
+++ Week1/Exercises/Day 4/Dependency Injection/src/com/java/academy/week1/day4/dependencyInjection/demo/Principal.java
@@ -0,0 +1,11 @@
+package com.java.academy.week1.day4.dependencyInjection.demo;
+
+public class Principal {
+
+    public static void main(String... puppies) {
+
+        System.out.println("Hello World!");
+
+    }
+
+}
