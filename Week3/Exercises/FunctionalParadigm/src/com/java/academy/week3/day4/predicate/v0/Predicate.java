77e710c2b7bc1f86560027fb723e0340e5752900
Victor Lavalle <vmlc1499@gmail.com>
2023-02-02 12:28:01 -0600

    🚧 Added functional code vs OOP exercises
    WIP

---
+++ Week3/Exercises/FunctionalParadigm/src/com/java/academy/week3/day4/predicate/v0/Predicate.java
@@ -0,0 +1,4 @@
+package com.java.academy.week3.day4.predicate.v0;
+
+public interface Predicate {
+}
---
+++ Week3/Exercises/FunctionalParadigm/src/com/java/academy/week3/day4/predicate/v0/Principal.java
@@ -0,0 +1,25 @@
+package com.java.academy.week3.day4.predicate.v0;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Principal {
+
+    private static final Employee[] arrayOfEmployees = {
+            new Employee(0, "Patrobas", 100.0),
+            new Employee(1, "Tercio", 200.0),
+            new Employee(2, "Andrónico", 300.0),
+            new Employee(3, "Epeneto", 400.0),
+            new Employee(4, "Filologo", 500.0),
+    };
+
+
+    public static void main(String[] args) {
+
+        List<Employee> listOfEmployees = Arrays.asList(arrayOfEmployees);
+
+    }
+
+
+}
+
