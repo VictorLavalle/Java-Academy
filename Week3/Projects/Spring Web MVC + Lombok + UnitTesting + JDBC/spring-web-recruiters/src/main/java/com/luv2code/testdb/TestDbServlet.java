61d2e22ee97bbce3e3d1b629339ca727d2757c99
Victor Lavalle <vmlc1499@gmail.com>
2023-02-03 02:34:09 -0600

    💩 Starting the project once again
    WIP

--- Week3/Projects/Spring Web MVC + Lombok + UnitTesting + JDBC/spring-web-recruiters/src/main/java/com/luv2code/testdb/TestDbServlet.java
+++ Week3/Projects/Spring Web MVC + Lombok + UnitTesting + JDBC/spring-web-recruiters/src/main/java/com/luv2code/testdb/TestDbServlet.java
@@ -28,7 +28,7 @@ public class TestDbServlet extends HttpServlet {
 		String pass = "springstudent";
 		
 		String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false";
-		String driver = "com.mysql.cj.jdbc.Driver";
+		String driver = "com.mysql.jdbc.Driver";
 		
 		// get connection to database
 		try {
