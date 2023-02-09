c4ac95de056891c8e26d868dd351d5de28a207a6
Victor Lavalle <vmlc1499@gmail.com>
2023-02-08 18:46:27 -0600

    🔥 Implement Observer in the REST API and a LOGGER
    WIP

--- Week4/Final Project/REST-Backend/src/main/java/com/java/academy/finalproject/RestBackendApplication.java
+++ Week4/Final Project/REST-Backend/src/main/java/com/java/academy/finalproject/RestBackendApplication.java
@@ -9,5 +9,4 @@ public class RestBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(RestBackendApplication.class, args);
     }
-
 }
--- Week4/Final Project/REST-Backend/src/main/java/com/java/academy/finalproject/rest/services/AcademyFormDataService.java
+++ Week4/Final Project/REST-Backend/src/main/java/com/java/academy/finalproject/rest/services/AcademyFormDataService.java
@@ -1,54 +1,60 @@
 package com.java.academy.finalproject.rest.services;
 
 import com.java.academy.finalproject.rest.entities.AcademyFormData;
+import com.java.academy.finalproject.rest.observer.DataObserved;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 import com.java.academy.finalproject.rest.repositories.AcademyFormDataRepository;
 
 @Service
-public class AcademyFormDataService {
+public class AcademyFormDataService extends DataObserved {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AcademyFormDataService.class);
+
+    private final AcademyFormDataRepository academyFormDataRepository;
 
     @Autowired
-    private AcademyFormDataRepository academyFormDataRepository;
+    public AcademyFormDataService(AcademyFormDataRepository academyFormDataRepository) {
+        this.academyFormDataRepository = academyFormDataRepository;
+    }
 
-
-    // This method is used in MainController to save the data to the database
-    public AcademyFormData saveAcademyFormData(
-            AcademyFormData academyFormData
-    ) {
-        switch (academyFormData.getRole()) {
-            case "0" -> academyFormData.setRole("Student");
-            case "1" -> academyFormData.setRole("Instructor");
-            case "2" -> academyFormData.setRole("Human Resources");
-            case "3" -> academyFormData.setRole("Other");
+    public AcademyFormData saveAcademyFormData(AcademyFormData academyFormData) {
+        try {
+            AcademyFormData savedAcademyFormData = academyFormDataRepository.saveAndFlush(academyFormData);
+            notifyObservers(savedAcademyFormData);
+            return savedAcademyFormData;
+        } catch (Exception e) {
+            LOGGER.error("Error saving academy form data", e);
+            throw e;
         }
-
-        return academyFormDataRepository.saveAndFlush(academyFormData);
     }
 
-
-    // This method is used in MainController to get all the data from the database
-    public List<AcademyFormData> getAcademyFormData() {
-        return academyFormDataRepository.findAll();
+    public List<AcademyFormData> getAllAcademyFormData() {
+        try {
+            return academyFormDataRepository.findAll();
+        } catch (Exception e) {
+            LOGGER.error("Error retrieving all academy form data", e);
+            throw e;
+        }
     }
 
-    // This method is used in MainController to update the data in the database
     public AcademyFormData updateAcademyFormData(long id, AcademyFormData academyFormData) {
-        return academyFormDataRepository.saveAndFlush(academyFormData);
+        AcademyFormData updatedAcademyFormData = academyFormDataRepository.saveAndFlush(academyFormData);
+        notifyObservers(updatedAcademyFormData);
+        return updatedAcademyFormData;
     }
 
-    // This method is used in MainController.java to delete a data from the database
     public void deleteAcademyFormData(long id) {
+        AcademyFormData deletedAcademyFormData = academyFormDataRepository.getReferenceById(id);
         academyFormDataRepository.deleteById(id);
+        notifyObservers(deletedAcademyFormData);
     }
 
-
-    // This method is used to delete thw whole data from the database
     public void deleteAcademyFormData(AcademyFormData academyFormData) {
         academyFormDataRepository.delete(academyFormData);
     }
-
 }
